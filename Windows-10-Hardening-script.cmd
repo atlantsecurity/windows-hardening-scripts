a70d812251d52b61e865008fe0aa87985c5e3252
Alexander Sverdlov <3330102+atlantsecurity@users.noreply.github.com>
2020-11-03 23:03:26 +0200

    Update Windows-10-Hardening-script.cmd

--- Windows-10-Hardening-script.cmd
+++ Windows-10-Hardening-script.cmd
@@ -13,6 +13,8 @@
 ::                        https://gist.github.com/ricardojba/ecdfe30dadbdab6c514a530bc5d51ef6
 ::
 ::###############################################################################################################
+:: Block tools which remotely install services, such as psexec!
+FOR /F "usebackq tokens=2 delims=:" %a IN (`sc.exe sdshow scmanager`) DO  sc.exe sdset scmanager D:(D;;GA;;;NU)%a
 :: Change file associations to protect against common ransomware and social engineering attacks.
 :: Note that if you legitimately use these extensions, like .bat, you will now need to execute them manually from cmd or powershell
 :: Alternatively, you can right-click on them and hit 'Run as Administrator' but ensure it's a script you want to run :) 
@@ -402,10 +404,11 @@ reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\policies\Explorer" /v No
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoAutorun /t REG_DWORD /d 1 /f
 ::
 :: Stop WinRM Service
-:: net stop WinRM
-:: reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WinRM\Service" /v AllowUnencryptedTraffic /t REG_DWORD /d 0 /f
+net stop WinRM
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WinRM\Service" /v AllowUnencryptedTraffic /t REG_DWORD /d 0 /f
 :: Disable WinRM Client Digiest authentication
-:: reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WinRM\Client" /v AllowDigest /t REG_DWORD /d 0 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WinRM\Client" /v AllowDigest /t REG_DWORD /d 0 /f
+net start WinRM
 :: Disabling RPC usage from a remote asset interacting with scheduled tasks
 reg add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Schedule" /v DisableRpcOverTcp /t REG_DWORD /d 1 /f
 :: Disabling RPC usage from a remote asset interacting with services
